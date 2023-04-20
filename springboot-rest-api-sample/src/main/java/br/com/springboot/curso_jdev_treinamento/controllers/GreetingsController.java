0298007a6e39a264cf8b8b61810ecb681f2a7e8d
Julio Costa <julio.github@hotmail.com>
2023-04-20 14:14:03 -0300

    Criado metodo para buscar nome

--- springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,7 +64,7 @@ public class GreetingsController {
     }
     
     @PostMapping(value = "salvar")
-    @ResponseBody
+    @ResponseBody/* descrição da resposta */
     public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) {
     	
     Usuario user =	usuarioRepository.save(usuario);
@@ -71,43 +74,92 @@ public class GreetingsController {
     	
     }
     
+    @PutMapping(value = "atualizar")
+    @ResponseBody/* descrição da resposta */
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario) {
+    	if (usuario.getId() == null) {
+    		return new ResponseEntity<String> ("Id não foi informado para atualização.", HttpStatus.OK);
+    	}
+    	
+    Usuario user =	usuarioRepository.saveAndFlush(usuario);
+    
+    return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+    	
+    	
+    }
     
     
     
     
+    @DeleteMapping(value = "delete")
+    @ResponseBody/* descrição da resposta */
+    public ResponseEntity<String> delete(@RequestParam Long iduser) { /* recebe os dados para deletar */
+    	
+    usuarioRepository.deleteById(iduser);
     
+    return new ResponseEntity<String>("User deletado com sucesso", HttpStatus.OK);
+    	
+    	
+    }
     
+    @GetMapping(value = "buscaruserid")
+    @ResponseBody/* descrição da resposta */
+    public ResponseEntity<Usuario> buscaruserid(@RequestParam(name = "iduser") Long iduser) { /* recebe os dados para atualizar ou consultar */
+    	
+    Usuario usuario = usuarioRepository.findById(iduser).get();
     
+    return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+    	
+    	
+    }
+   
+    @GetMapping(value = "buscarPorNome")
+    @ResponseBody/* descrição da resposta */
+    public ResponseEntity<List<Usuario>> buscarPorNome(@RequestParam(name = "name") String name) { /* recebe os dados para atualizar ou consultar */
+    	
+    List<Usuario> usuario= usuarioRepository.buscarPorNome(name);
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    return new ResponseEntity<List<Usuario>>(usuario, HttpStatus.OK);
+    	
+    	
+    }
     
     
 }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
--- springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/repository/UsuarioRepository.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/repository/UsuarioRepository.java
@@ -1,6 +1,9 @@
 package br.com.springboot.curso_jdev_treinamento.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.springboot.curso_jdev_treinamento.model.Usuario;
@@ -9,5 +12,7 @@ import br.com.springboot.curso_jdev_treinamento.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
 	
+	@Query(value = "select u from Usuario u where u.nome like %?1%")
+	List<Usuario> buscarPorNome(String name);
 
 }
